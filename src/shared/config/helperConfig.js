9e2776d602500079ff7a9b8aced66301cb74f45e
Lakshitha <lweekrakkody@expedia.com>
2018-09-02 02:23:29 +0530

    Message splitting functionality and unit tests for it

---
+++ src/shared/config/helperConfig.js
@@ -0,0 +1,6 @@
+const MessageSplitConfig = {
+	messageCharcaterLimit : 50,
+	partIndicatorBaseMask : "XX"
+}
+
+module.exports = {MessageSplitConfig}
--- src/shared/containers/message/Message.js
+++ src/shared/containers/message/Message.js
@@ -1,22 +1,33 @@
 import React from 'react'
-
+import * as Helpers from '../../common/helperFunctions'
 
 export default class Message extends React.Component{
 	constructor(){
 		super()
+		this.state = {
+			txtMessage: ""
+		}
 	}
 
 	static requestInitialdata(params){
 		return	Promise.resolve()//Promise.all([params.dispatch(sampleActions.loadIntro())])
 	}
 
-	render(){
+	typeMessage(e){
+		this.setState({txtMessage: e.target.value})
+	}
 
+	sendMessage(){
+		console.log('sending message ' + this.state.txtMessage)
+	}
+
+	render(){
+		
 		return(
 			<div class="main_container">
 					<h1>Tweeter</h1>
-					<textarea id="message" class="text"></textarea>
-					<button type="button" id="submit" class="button">Send</button>
+					<textarea id="message" class="text" value={this.state.txtMessage} onChange={this.typeMessage.bind(this)}></textarea>
+					<button type="button" id="submit" class="button" onClick={this.sendMessage.bind(this)}>Send</button>
 			</div>
 		)
 	}
